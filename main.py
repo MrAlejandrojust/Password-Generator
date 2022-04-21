14d6484ba8e973efcdaf0a525f99a57eb5aa8ea9
Alejandro Osorio <al3jandrojust@gmail.com>
2022-04-21 18:44:17 -0500

    MyProject-0.5-Jack and the Beanstalk

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-import os
-import sys
-import time
-
-
-def typingPrint(text):
-    for character in text:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.05)
-
-
-def typingInput(text):
-    for character in text:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.05)
-    value = input()
-    return value
-
-
-def clearScreen():
-    os.system("cls")
+#
 
 
 def main():
-    print('ola')
+    bean_height = 100
+
+    try:
+        hours = input('Enter the number of hours: ')
+        hours = int(hours)
+
+        if hours < 0:
+            print('Please enter a positive number')
+            quit()
+        else:
+            for i in range(0, hours - 1):
+                bean_height = bean_height * 1.5 + 30
+
+            bean_height = bean_height / 100
+            bean_height = "{0:.2f}".format(bean_height)
+
+            print('After {} Hours, the beanstalk was {} meters tall!'.format(hours, bean_height))
+    except ValueError:
+        print('Please enter a valid number of Hours')
+        main()
 
 
 if __name__ == '__main__':
