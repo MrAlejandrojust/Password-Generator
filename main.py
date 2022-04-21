21bc1ab502e0773f8e27a0aa626999c35e1b63d3
Alejandro Osorio <al3jandrojust@gmail.com>
2022-04-21 15:26:10 -0500

    MyProject-0.4

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-# import
+import os
+import sys
+import time
+
+
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def typingInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    value = input()
+    return value
+
+
+def clearScreen():
+    os.system("cls")
+
 
 def main():
-    print('Now is working')
+    print('ola')
 
 
 if __name__ == '__main__':
