1e26d5e7686e68f3a997cce7af9ae667538d3284
Alejandro Osorio <al3jandrojust@gmail.com>
2022-04-25 15:51:21 -0500

    MyProject-0.6-Random Password Generator

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-#
+import random
+
+
+def shuffle(string):
+    temp_list = list(string)
+    random.shuffle(temp_list)
+    return ''.join(temp_list)
 
 
 def main():
-    bean_height = 100
+    upper_case_letter1 = chr(random.randint(65, 90))
+    upper_case_letter2 = chr(random.randint(65, 90))
+    lower_case_letter1 = chr(random.randint(97, 122))
+    lower_case_letter2 = chr(random.randint(97, 122))
+    digit1 = str(random.randint(0, 9))
+    digit2 = str(random.randint(0, 9))
+    punctuation_sign1 = chr(random.randint(33, 152))
+    punctuation_sign2 = chr(random.randint(33, 152))
 
-    try:
-        hours = input('Enter the number of hours: ')
-        hours = int(hours)
+    password = shuffle(upper_case_letter1 + upper_case_letter2 + lower_case_letter1 + lower_case_letter2 + digit1 +
+                       digit2 + punctuation_sign1 + punctuation_sign2)
 
-        if hours < 0:
-            print('Please enter a positive number')
-            quit()
-        else:
-            for i in range(0, hours - 1):
-                bean_height = bean_height * 1.5 + 30
-
-            bean_height = bean_height / 100
-            bean_height = "{0:.2f}".format(bean_height)
-
-            print('After {} Hours, the beanstalk was {} meters tall!'.format(hours, bean_height))
-    except ValueError:
-        print('Please enter a valid number of Hours')
-        main()
+    print(password)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,11 +1,54 @@
-# import
+import sys
+import time
+import os
+
+
+def power2(n):
+    # n = int(input('Enter a number: '))
+    n = n
+
+    if n > 0 and (n & n - 1) == 1:
+        print('Is power of 2')
+    else:
+        print("Isn't power of 2")
+
+
+def mult(a, b):
+    a = int(a)
+    b = int(b)
+    product = 0
+
+    for c in range(1, b + 1, 1):
+        product = product + a
+
+    print(f'Result: {product}')
+
+
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def typingInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    value = input()
+    return value
+
+
+def clearScreen():
+    os.system("cls")
+
 
 def test():
-
     fhand = input('Please enter file path: ')
     try:
         fhand = open(fhand)
-    except:
+    except SyntaxError:
         print('File has not been found in path.')
         quit()
 
